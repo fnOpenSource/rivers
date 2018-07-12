5a59c38f456ea498b029605f3743e213ec36aea6
悟空前传 <sprig.zhang@SH-CODE-PC0090.fn.com>
2018-07-12 14:15:03 +0800

    update

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 #OPTS
 JAVA_OPTS="$JAVA_OPTS -server -Xms2g -Xmx2g -Xmn512M -XX:+UseConcMarkSweepGC -XX:ParallelGCThreads=8 -XX:+HeapDumpOnOutOfMemoryError"
 
-nohup ${JAVA_HOME}/bin/java ${JAVA_OPTS} -jar  river.jar &
+nohup ${JAVA_HOME}/bin/java ${JAVA_OPTS} -jar  rivers.jar > nohup.txt 2>&1 &
